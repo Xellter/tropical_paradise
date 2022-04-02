3c95fd8f31be9ec64e17ed972155d859cb2fcb58
Przemek <prz.konopko@gmail.com>
2022-04-02 16:59:37 +0200

    edited main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const nav = document.querySelector(".nav");
 const burgerBtn = document.querySelector(".burger-btn");
 const navItems = document.querySelectorAll(".nav__item");
+const navBtnBars = document.querySelector(".burger-btn__bars");
+const allSections = document.querySelectorAll(".section");
+const footerYear = document.querySelector(".footer__year");
 
 const showNav = () => {
 	nav.classList.toggle("nav--active");
+	navBtnBars.classList.remove("black-bars-color");
 
 	navItems.forEach((item) => {
 		item.addEventListener("click", () => {
@@ -21,5 +25,30 @@ const handleNavItemsAnimation = () => {
 		delayTime++;
 	});
 };
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.textContent = year;
+};
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.add("black-bars-color");
+		} else if (
+			!section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.remove("black-bars-color");
+		}
+	});
+};
+
+handleCurrentYear();
 
 burgerBtn.addEventListener("click", showNav);
+window.addEventListener("scroll", handleObserver);
